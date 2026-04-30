6afeea4b96eb968c6a6931ae5b38a3a405ae13c8
Sarthak Pandey <pandeysarthak06@gmail.com>
2026-04-30 11:54:10 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 // Change this to switch between local and production
-const IS_PRODUCTION = false;
+const IS_PRODUCTION = true;
 
 export const API_URL = IS_PRODUCTION
   ? "https://agrifriend-backend.onrender.com"
   : "http://localhost:8000";
-  
